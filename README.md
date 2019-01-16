b26808ede849cc5c418f99b3214464ac13937da5
Freddie <sfm1617@hotmail.com>
2019-01-16 23:32:12 +0000

    added subs api

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ How to get started:
 2. `npm install`
 3. `npm start` OR start the server with a long-running daemon like `supervisor` or `nodemon`
 
+You can send `email` and `name` to the mock api endpoint `/api/subscribe` to check the response.
+
 *You set up your own middleware as you needed. Please mind any vulnerability.*
 
 Happy Coding 😜
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
+const { isEmail } = require('validator');
 
 app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.post('/api/subscribe', (req, res, next) => {
+    const { email ='', name='', ...params } = req.body;
+    if (!isEmail(email)) {
+        res.status(400).json({
+            errors: [{ pointer: 'email', message: 'Email is invalid' }],
+        });
+        return;
+    }
+    if (name === '') {
+        res.status(400).json({
+            errors: [{ pointer: 'name', message: 'name is required' }],
+        });
+        return;
+    }
+    res.status(201).json({
+        'subscribe': 'success'
+    });
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
